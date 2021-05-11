58aaa612b22cf1d7d46a538839acf9b86d957b84
jiazhi.xie 谢佳智 <jiazhi_xie@wishbuild.com>
2021-05-11 16:19:16 +0800

    feign集成了hystrix熔断，增加配置，写回调方法进行配置即可开启

--- spring-cloud-hystrix/spring-cloud-consumer-hystrix/src/main/java/com/neo/remote/HelloRemote.java
+++ spring-cloud-hystrix/spring-cloud-consumer-hystrix/src/main/java/com/neo/remote/HelloRemote.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 /**
  * Created by summer on 2017/5/11.
  */
+//这里的在HelloRemote类添加指定fallback类，在服务熔断的时候返回fallback类中的内容。
 @FeignClient(name= "spring-cloud-producer", fallback = HelloRemoteHystrix.class)
 public interface HelloRemote {
 
--- spring-cloud-hystrix/spring-cloud-consumer-hystrix/src/main/java/com/neo/remote/HelloRemoteHystrix.java
+++ spring-cloud-hystrix/spring-cloud-consumer-hystrix/src/main/java/com/neo/remote/HelloRemoteHystrix.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
  * Created by summer on 2017/5/15.
  */
 @Component
+//创建HelloRemoteHystrix类继承与HelloRemote实现回调的方法
 public class HelloRemoteHystrix implements HelloRemote{
 
     @Override
